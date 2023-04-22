14a3b9a2e36b605c136a90bbcb2e0b681e3b1c9d
Ali <ali512@outlook.com>
2023-04-22 20:00:34 +0300

    pages layout

--- my-app/pages/layout.tsx
+++ my-app/pages/layout.tsx
@@ -9,16 +9,21 @@ export default function RootLayout({ children }) {
     return (
 
         <>
-            <div className="container-fluid">
-                <div className="row flex-nowrap">
-                    <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                        <SideBar></SideBar>
-                    </div>
-                    <div className="col py-3">
-                        {children}
+            <div className="container-fluid overflow-hidden">
+                    <div className="row vh-100 overflow-auto">
+                        <div className="col-12 col-sm-4 col-md-3 col-xl-2 px-sm-2 px-0 bg-dark d-flex sticky-top">
+                            <SideBar></SideBar>
+                        </div>
+                        <div className="col d-flex flex-column h-sm-100" style={{minHeight: 'calc(100% - 62px)'}}>
+                            <main className="overflow-auto pt-3 px-3">
+                                {children}
+                            </main>
+                            <footer className="bg-light py-4 mt-auto">
+                                Footer content here...
+                            </footer>
+                        </div>
                     </div>
                 </div>
-            </div>
 
         </>
     )
